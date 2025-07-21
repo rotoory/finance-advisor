b3ce695b85f08970118b89f7ac81a0d22ee1b79b
rotoory <rotoory@gmail.com>
2025-07-21 18:32:33 +0000

    Fix: Corrected font import in _app.js for Next.js 13+

--- finance-advisor-landing/src/pages/_app.js
+++ finance-advisor-landing/src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 
